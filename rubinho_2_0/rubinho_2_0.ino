1a53cef368af643c5c24cb7351dd202a2116b5a0
Raissa Camelo <raissa_camelo@hotmail.com>
2016-10-31 10:45:27 -0300

    Atualização nas portas

---
+++ rubinho_2_0/rubinho_2_0.ino
@@ -0,0 +1,184 @@
+/*int le = 7;*
+int lm = 6;*
+int ld = 5;*
+int lsc = 4;*
+*/
+
+int md2 = 7;
+int md1 = 8;
+int me1 = 13;
+int me2 = 12;
+
+int sm = A1;
+int sd = A2;
+int se = A0;
+int spc = A3;
+
+int smRead;
+int seRead;
+int sdRead;
+int spcRead;
+
+//int meio = 511;
+int meio = 600;
+int flagPC = 0;
+
+int flagCruzamento = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(sm, INPUT);
+  pinMode(sd, INPUT);
+  pinMode(se, INPUT);
+  pinMode(spc, INPUT);
+  pinMode(md1, OUTPUT);
+  pinMode(md2, OUTPUT);
+  pinMode(me1, OUTPUT);
+  pinMode(me2, OUTPUT);
+  pinMode(le, OUTPUT);
+  pinMode(lm, OUTPUT);
+  pinMode(ld, OUTPUT);
+  pinMode(lsc, OUTPUT);
+  Serial.begin(9600);
+  digitalWrite(le, HIGH);
+  digitalWrite(ld, HIGH);
+  digitalWrite(lm, HIGH);
+  digitalWrite(lsc, HIGH);
+}
+
+void loop() {
+        
+  
+        
+  smRead = analogRead(sm);
+  seRead = analogRead(se);
+  sdRead = analogRead(sd);
+  spcRead = digitalRead(spc);
+  
+   
+//    if ((flagPC ==0) && (spcRead ==1) && (smRead >meio)&&(seRead <meio)&&(sdRead<meio)){
+//       flagPC = 1; 
+//    }
+//    else if ((flagPC == 1) && (spcRead==0) && (smRead >meio)&&(seRead <meio)&&(sdRead<meio)){
+//        flagPC = 2;
+//    }
+//    else if ( (flagPC ==2) && (spcRead ==1) && (smRead >meio)&&(seRead <meio)&&(sdRead<meio)){
+//      flagPC = 3;
+//     delay(1500);
+//      off();  
+//      delay(20000);
+//  }
+        //Controle partida chegada
+         if ((flagCruzamento == 0) && (spcRead == 1)) {   //largada
+           flagCruzamento = 1;      //Estado 1
+           
+         }
+         else if ((flagCruzamento ==1) && (spcRead == 0)){
+            flagCruzamento = 4; 
+         }
+         
+         else if ((seRead > meio) && (smRead>meio) &&(sdRead>meio)){   //Cruzamento  - sensor do meio
+            if (flagCruzamento == 4){
+                 flagCruzamento = 2; 
+            }
+          }
+          
+          else if ((flagCruzamento == 2) && (spcRead == 1)){      //Cruzamento - sensor traseiro
+                 flagCruzamento = 3; 
+          }
+          else if ((flagCruzamento == 3 ) && (spcRead==0)) { //cruzamento
+                flagCruzamento = 4;
+          }
+          else if ((spcRead == 1) && (flagCruzamento == 4)){         //Chegada
+            off();  
+            delay(20000);
+            
+          }
+          
+          
+          //Controle das curvas
+      
+          if((smRead > seRead && smRead > sdRead && sdRead < meio && seRead < meio) || (smRead > meio && sdRead > meio && seRead > meio))
+          {
+            forward();
+
+          }
+          else if ((seRead > smRead && seRead > sdRead) || (seRead > sdRead && smRead > sdRead && smRead > meio))
+          {
+            left();
+          }
+          else if((sdRead > seRead && sdRead > smRead) || (smRead > seRead && sdRead > seRead && smRead > meio))
+          {
+            right();
+          }
+          
+          else if(smRead < meio && sdRead < meio && seRead < meio)
+          {
+              backward();
+          }  
+
+     
+    
+ 
+    
+  Serial.print("sm ");
+  Serial.println(smRead);
+  Serial.print("se"); 
+  Serial.println(seRead);
+  Serial.print("sd ");
+  Serial.println(sdRead);
+  Serial.println("");
+  
+  Serial.println("spc");
+  Serial.print(spcRead);
+  Serial.println("");
+  Serial.print("contador ");
+  Serial.println(flagCruzamento);
+
+
+}
+
+
+void forward()
+{
+  digitalWrite(md1, HIGH);
+  digitalWrite(md2, LOW);
+  digitalWrite(me1, HIGH);
+  digitalWrite(me2, LOW);
+
+}
+
+void left()
+{
+  digitalWrite(md1, LOW);
+  digitalWrite(md2, HIGH);//MUDEI AQUI PRA LOW
+  digitalWrite(me1, HIGH);
+  digitalWrite(me2, LOW);
+
+}
+
+void right()
+{
+    digitalWrite(md1, HIGH);
+    digitalWrite(md2, LOW);
+    digitalWrite(me1, LOW);
+    digitalWrite(me2, HIGH);//MUDEI AQUI PEA LOW 
+}
+
+void backward()
+{
+    digitalWrite(md1, LOW);
+  digitalWrite(md2, HIGH);
+    digitalWrite(me1, LOW);
+    digitalWrite(me2, HIGH);
+}  
+
+void off()
+{
+  digitalWrite(md1, LOW);
+  digitalWrite(md2, LOW);
+  digitalWrite(me1, LOW);
+  digitalWrite(me2, LOW);
+}
+
+
